d3a040b54b4e9ca19767d7a7c80c4d0b353d80f5
Sebastian Balanta <familia BS@familiaBS>
2017-04-25 16:23:30 -0500

    Resta. Sebastian Balanta

--- Calculadora/src/tallergitnetbeans/TallerGitNetbeans.java
+++ Calculadora/src/tallergitnetbeans/TallerGitNetbeans.java
@@ -14,6 +14,7 @@
     Responsabilidad: Clase main para inicializar la interfaz de una calculadora
 
     Colaboracion: ninguna
+
     LEER
     Hice unas modificaciones, si no entienden algo me avisan, no vayan a cambiar todo solo
     porque si, recuerden hacer la comprobacion de valores para la division y el resto
@@ -27,9 +28,41 @@ public class TallerGitNetbeans {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        CalculadoraSimple interfaz = new CalculadoraSimple();
-        interfaz.setVisible(true);
+    
+           public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(CalculadoraSimple.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(CalculadoraSimple.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(CalculadoraSimple.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(CalculadoraSimple.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new CalculadoraSimple().setVisible(true);
+                
+            }
+        });
     }
+
+        
+    
     
 }
